171df56b910efb637af4567062ef387a9cd49e42
ariana <arianawhatudoinghere@gmail.com>
2024-03-12 08:59:23 -0300

    first-commit

---
+++ src/main/kotlin/Exemplos/ExemploReadLine.kt
@@ -0,0 +1,18 @@
+package Exemplos
+
+fun multiplicar(x: Int, y: Int): Int{
+    return x * y
+}
+
+fun main(args: Array<String>){
+
+    println("Digite o primeiro número: ")
+    val x = readLine()!!.toInt()
+
+    println("Digite o segundo número: ")
+    val y = readLine()!!.toInt()
+
+    val resultado = multiplicar(x,y)
+
+    println("O resultado da soma é  = $resultado")
+}
